2885fba6aff04e0f33d678fe0a8b41f4e41be481
brysenPfingsten <brysen.pfingsten@gmail.com>
2025-09-01 15:52:53 -0400

    Vimtex and spelling

---
+++ .config/nvim/lua/plugins/vimtex.lua
@@ -0,0 +1,33 @@
+return {
+  "lervag/vimtex",
+  ft = { "tex" },
+  init = function()
+    -- Viewer: choose one you actually use
+    -- 'zathura', 'sioyek', 'skim', 'okular', 'general', 'sumatrapdf'
+    vim.g.vimtex_view_method = "zathura"  -- macOS: "skim"; Windows: "sumatrapdf"
+
+    -- Compiler: latexmk (continuous)
+    vim.g.vimtex_compiler_method = "latexmk"
+    vim.g.vimtex_compiler_latexmk = {
+      build_dir = "build",       -- put aux/pdf in ./build
+      aux_dir = "build",
+      continuous = 1,            -- -pvc
+      callback = 1,              -- let vimtex notify viewer on updates
+      executable = "latexmk",
+      options = {
+        "-pdf",                  -- or "-pdflua" / "-xelatex"
+        "-interaction=nonstopmode",
+        "-synctex=1",
+        "-file-line-error",
+        "-shell-escape",
+      },
+    }
+
+    -- Let vimtex communicate with nvim via neovim-remote (needed for inverse search)
+    vim.g.vimtex_compiler_progname = "nvr"
+
+    -- Nice-to-haves
+    vim.g.vimtex_quickfix_mode = 0        -- don't auto-open quickfix on warnings
+    vim.g.tex_flavor = "latex"
+  end,
+}
