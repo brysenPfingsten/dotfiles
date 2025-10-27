993469a0032341c49f19eaaf565b1bb754a34e4e
Brysen Pfingsten <brysen.pfingsten@gmail.com>
2025-10-26 20:47:40 -0400

    power menu works

--- nix/modules/waybar/scripts/power-menu.sh
+++ nix/modules/waybar/scripts/power-menu.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
-picker() { rofi --dmenu -i -p "Power"; }
 
-choice=$(printf " Lock\n Suspend\n Reboot\n Shutdown" | picker)
+# Use -dmenu (one dash), not --dmenu
+picker() { rofi -dmenu -i -p "Power"; }
+
+choice="$(printf '%s\n' ' Lock' ' Suspend' ' Reboot' ' Shutdown' | picker)" || exit 0
 
 case "$choice" in
 " Lock") exec gtklock || exec swaylock ;;
 " Suspend") exec systemctl suspend ;;
 " Reboot") exec systemctl reboot ;;
 " Shutdown") exec systemctl poweroff ;;
+*) exit 0 ;;
 esac
