09d49a1c3958d7e7a1e8f51450816ae8a88d3d03
brysenPfingsten <brysen.pfingsten@gmail.com>
2026-01-12 11:13:16 -0500

    No more declaring neovim
    Fuck it dynamic linking and real lazy vim

--- neovim/lua/plugins/neotest.lua
+++ neovim/lua/plugins/neotest.lua
@@ -5,14 +5,13 @@ return {
     "nvim-treesitter/nvim-treesitter",
     "nvim-neotest/neotest-python",
   },
-  dev = true,
   config = function()
     require("neotest").setup({
       adapters = {
         require("neotest-python")({
           -- by default uses pytest if available
           dap = { justMyCode = false },
-          args = { "-q" },    -- add default pytest args
+          args = { "-q" }, -- add default pytest args
           runner = "pytest",
         }),
       },
@@ -22,11 +21,23 @@ return {
 
     local nt = require("neotest")
     -- Keymaps
-    vim.keymap.set("n", "<leader>tn", function() nt.run.run() end, { desc = "Test: nearest" })
-    vim.keymap.set("n", "<leader>tf", function() nt.run.run(vim.fn.expand("%")) end, { desc = "Test: file" })
-    vim.keymap.set("n", "<leader>ts", function() nt.run.stop() end, { desc = "Test: stop" })
-    vim.keymap.set("n", "<leader>to", function() nt.output.open({ enter = true, auto_close = true }) end, { desc = "Test: output" })
-    vim.keymap.set("n", "<leader>tS", function() nt.summary.toggle() end, { desc = "Test: summary" })
-    vim.keymap.set("n", "<leader>ta", function() nt.run.run({ suite = true }) end, { desc = "Test: all" })
+    vim.keymap.set("n", "<leader>tn", function()
+      nt.run.run()
+    end, { desc = "Test: nearest" })
+    vim.keymap.set("n", "<leader>tf", function()
+      nt.run.run(vim.fn.expand("%"))
+    end, { desc = "Test: file" })
+    vim.keymap.set("n", "<leader>ts", function()
+      nt.run.stop()
+    end, { desc = "Test: stop" })
+    vim.keymap.set("n", "<leader>to", function()
+      nt.output.open({ enter = true, auto_close = true })
+    end, { desc = "Test: output" })
+    vim.keymap.set("n", "<leader>tS", function()
+      nt.summary.toggle()
+    end, { desc = "Test: summary" })
+    vim.keymap.set("n", "<leader>ta", function()
+      nt.run.run({ suite = true })
+    end, { desc = "Test: all" })
   end,
 }
--- neovim/lua/plugins/snips.lua
+++ neovim/lua/plugins/snips.lua
@@ -1,47 +1,46 @@
 return {
-	{
-		"L3MON4D3/LuaSnip",
-		dependencies = { "rafamadriz/friendly-snippets" },
-		build = "make install_jsregexp",
-		dev = false,
-		config = function()
-			local luasnip = require("luasnip")
-			local types = require("luasnip.util.types")
+  {
+    "L3MON4D3/LuaSnip",
+    dependencies = { "rafamadriz/friendly-snippets" },
+    build = "make install_jsregexp",
+    config = function()
+      local luasnip = require("luasnip")
+      local types = require("luasnip.util.types")
 
-			-- Load snippets from vscode-style and your own folder
-			require("luasnip.loaders.from_vscode").lazy_load()
-			require("luasnip.loaders.from_lua").lazy_load({ paths = "~/.config/nvim/snippets" })
-			require("luasnip.loaders.from_snipmate").lazy_load({ paths = "~/.config/nvim/snippets" })
+      -- Load snippets from vscode-style and your own folder
+      require("luasnip.loaders.from_vscode").lazy_load()
+      require("luasnip.loaders.from_lua").lazy_load({ paths = "~/.config/nvim/snippets" })
+      require("luasnip.loaders.from_snipmate").lazy_load({ paths = "~/.config/nvim/snippets" })
 
-			-- Snippet navigation
-			vim.keymap.set({ "i", "s" }, "<C-g>", function()
-				if luasnip.expand_or_jumpable() then
-					luasnip.expand_or_jump()
-				end
-			end, { silent = true, desc = "Expand or jump in snippet" })
+      -- Snippet navigation
+      vim.keymap.set({ "i", "s" }, "<C-g>", function()
+        if luasnip.expand_or_jumpable() then
+          luasnip.expand_or_jump()
+        end
+      end, { silent = true, desc = "Expand or jump in snippet" })
 
-			vim.keymap.set({ "i", "s" }, "<C-j>", function()
-				if luasnip.jumpable(-1) then
-					luasnip.jump(-1)
-				end
-			end, { silent = true, desc = "Jump backwards in snippet" })
+      vim.keymap.set({ "i", "s" }, "<C-j>", function()
+        if luasnip.jumpable(-1) then
+          luasnip.jump(-1)
+        end
+      end, { silent = true, desc = "Jump backwards in snippet" })
 
-			-- Choice nodes (cycle with Ctrl-l)
-			vim.keymap.set("i", "<C-l>", function()
-				if luasnip.choice_active() then
-					luasnip.change_choice(1)
-				end
-			end, { silent = true, desc = "Cycle snippet choice" })
+      -- Choice nodes (cycle with Ctrl-l)
+      vim.keymap.set("i", "<C-l>", function()
+        if luasnip.choice_active() then
+          luasnip.change_choice(1)
+        end
+      end, { silent = true, desc = "Cycle snippet choice" })
 
-			-- Global settings
-			luasnip.config.set_config({
-				history = true,
-				updateevents = "TextChanged,TextChangedI",
-				enable_autosnippets = true,
-				ext_opts = {
-					[types.choiceNode] = { active = { virt_text = { { "<-", "Comment" } } } },
-				},
-			})
-		end,
-	},
+      -- Global settings
+      luasnip.config.set_config({
+        history = true,
+        updateevents = "TextChanged,TextChangedI",
+        enable_autosnippets = true,
+        ext_opts = {
+          [types.choiceNode] = { active = { virt_text = { { "<-", "Comment" } } } },
+        },
+      })
+    end,
+  },
 }
