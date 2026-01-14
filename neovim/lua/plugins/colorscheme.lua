14ce2466ca861bcb4e9c771edccac80bf1835698
brysenPfingsten <brysen.pfingsten@gmail.com>
2026-01-14 10:41:56 -0500

    Going back to onedark for neovim instead of pywal

--- neovim/lua/plugins/colorscheme.lua
+++ neovim/lua/plugins/colorscheme.lua
@@ -1,21 +1,20 @@
+return {
+  "navarasu/onedark.nvim",
+  priority = 1000,
+  config = function()
+    require("onedark").setup({
+      style = "warmer",
+      transparent = true,
+    })
+    require("onedark").load()
+  end,
+}
 -- return {
--- 	"navarasu/onedark.nvim",
--- 	priority = 1000,
+-- 	"uZer/pywal16.nvim",
+-- 	name = "pywal16",
 -- 	config = function()
--- 		require("onedark").setup({
--- 			style = "darker",
--- 			transparent = true,
--- 			-- term_colors = true, -- ensures Neovim’s terminal colors match
--- 		})
--- 		require("onedark").load()
+-- 		local pywal16 = require("pywal16")
+-- 		pywal16.setup()
+-- 		vim.cmd.colorscheme("pywal16")
 -- 	end,
 -- }
-return {
-	"uZer/pywal16.nvim",
-	name = "pywal16",
-	config = function()
-		local pywal16 = require("pywal16")
-		pywal16.setup()
-		vim.cmd.colorscheme("pywal16")
-	end,
-}
