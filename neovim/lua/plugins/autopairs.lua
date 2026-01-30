7234d53a90f69de192fe52e417a1346af6985816
brysenPfingsten <brysen.pfingsten@gmail.com>
2026-01-30 16:38:51 -0500

    Neovim: Auto pair for $ in typst

--- neovim/lua/plugins/autopairs.lua
+++ neovim/lua/plugins/autopairs.lua
@@ -9,6 +9,12 @@ return {
           vim.keymap.set("i", "`", "`", { buffer = true })
         end,
       })
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = { "typst" },
+        callback = function(ev)
+          vim.keymap.set("i", "$", "$$<Left>", { buffer = true, noremap = true })
+        end,
+      })
 
       return opts
     end,
