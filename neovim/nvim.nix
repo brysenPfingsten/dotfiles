4f0269a4b8d7af131977e3bb61a1b5f2c2705e3b
Brysen Pfingsten <brysen.pfingsten@gmail.com>
2026-02-20 17:20:54 -0500

    Neovim / Nix: Moving languages, formatters, and lsps to neovim file

--- neovim/nvim.nix
+++ neovim/nvim.nix
@@ -1,4 +1,51 @@
-{...}: {
+{pkgs, ...}: {
+  home.packages = with pkgs; [
+    # Nix
+    nixd
+    alejandra
+    # Lua
+    stylua
+    lua-language-server
+    # Typst
+    typst
+    tinymist
+    typstyle
+    # C
+    clang-tools
+    lldb
+    # Racket
+    racket
+    # Python
+    python3
+    python313Packages.pytest_7
+    pyright
+    ruff
+    black
+    uv
+    # Javascript / Typescript
+    nodejs
+    typescript-language-server
+    # SAT
+    z3
+    graphviz
+    # Rust
+    rust-analyzer
+    rustc
+    cargo
+    clippy
+    rustfmt
+    pkg-config
+    # LaTeX
+    texlive.combined.scheme-full
+    fontconfig
+
+    # Development
+    tree-sitter
+    xclip
+    lazydocker
+    docker
+    docker-compose
+  ];
   programs.neovim.enable = true;
   xdg.configFile."nvim" = {
     source = ./.;
--- packages/packages.nix
+++ packages/packages.nix
@@ -1,57 +1,5 @@
-{pkgs, ...}: let
-  codelldb = pkgs.writeShellScriptBin "codelldb" ''
-    exec ${pkgs.vscode-extensions.vadimcn.vscode-lldb}/share/vscode/extensions/vadimcn.vscode-lldb/adapter/codelldb "$@"
-  '';
-in {
+{pkgs, ...}: {
   home.packages = with pkgs; [
-    # Languages, Language Servers and Formatters
-    # C
-    clang-tools
-    lldb
-    codelldb
-    # Nix
-    nixd
-    alejandra
-    # Racket
-    racket
-    # racket-langserver # Racket
-    # raco-fmt
-    lua-language-server
-    # Lua
-    stylua
-    # Python
-    python3
-    python313Packages.pytest_7
-    pyright
-    ruff
-    black
-    uv
-    # Javascript
-    nodejs
-    # Rust
-    rust-analyzer
-    rustc
-    cargo
-    clippy
-    rustfmt
-    pkg-config
-    # Javascript / Typescript
-    typescript-language-server
-    # SAT
-    z3
-    graphviz
-    # LaTeX
-    texlive.combined.scheme-full
-    fontconfig
-    typst
-
-    # Development
-    tree-sitter
-    xclip
-    lazydocker
-    docker
-    docker-compose
-
     # CLIs
     unzip
     zoxide
@@ -66,7 +14,6 @@ in {
     codex
 
     # Web Apps
-    chromium
     wasistlos
 
     # GUIs
@@ -76,7 +23,6 @@ in {
 
     # PDFs
     zathura
-    pympress
     foliate
 
     # Terminal and TUIs
