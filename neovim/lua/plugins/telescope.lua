dfc329d1f91fdb655a94d577c6a5fb9b3cb94607
Brysen Pfingsten <brysen.pfingsten@gmail.com>
2026-03-07 23:16:16 -0500

    Neovim: Telescope git file history

--- neovim/lua/plugins/telescope.lua
+++ neovim/lua/plugins/telescope.lua
@@ -1,27 +1,29 @@
 return {
-	"nvim-telescope/telescope.nvim",
-	version = "*",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-	},
-	config = function()
-		local telescope = require("telescope.builtin")
-		local map = vim.keymap.set
+  "nvim-telescope/telescope.nvim",
+  version = "*",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+  },
+  config = function()
+    local telescope = require("telescope.builtin")
+    local map = vim.keymap.set
 
-		-- Normal Fuzzy Finding
-		map("n", "<space>ff", telescope.find_files)
+    -- Normal Fuzzy Finding
+    map("n", "<space>ff", telescope.find_files)
 
-		-- Editing Neovim Config
-		map("n", "<space>en", function()
-			telescope.find_files({
-				cmd = vim.fn.stdpath("config"),
-			})
-		end)
+    -- Editing Neovim Config
+    map("n", "<space>en", function()
+      telescope.find_files({
+        cmd = vim.fn.stdpath("config"),
+      })
+    end)
 
-		-- Grep for words
-		vim.keymap.set("n", "<leader>fg", telescope.live_grep, {})
-		-- Buffers
-		vim.keymap.set("n", "<leader>fb", telescope.buffers, {})
-	end,
+    -- Grep for words
+    vim.keymap.set("n", "<leader>fg", telescope.live_grep, {})
+    -- Buffers
+    vim.keymap.set("n", "<leader>fb", telescope.buffers, {})
+    -- Git File History
+    vim.keymap.set("n", "<leader>fh", telescope.git_bcommits, {})
+  end,
 }
