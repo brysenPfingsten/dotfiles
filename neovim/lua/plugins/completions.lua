1b994d28626cb8020a04b823b9aed0c1bfe90e75
Brysen Pfingsten <brysen.pfingsten@gmail.com>
2026-04-23 09:57:50 -0400

    Neovim: Alt for completions

--- neovim/lua/plugins/completions.lua
+++ neovim/lua/plugins/completions.lua
@@ -59,25 +59,25 @@ return {
         }),
       })
 
-      vim.keymap.set({ "i", "s" }, "<C-k>", function()
+      vim.keymap.set({ "i", "s" }, "<M-k>", function()
         if ls.expand_or_jumpable() then
           ls.expand_or_jump()
         end
       end, { silent = true, desc = "LuaSnip expand or jump" })
 
-      vim.keymap.set({ "i", "s" }, "<C-j>", function()
+      vim.keymap.set({ "i", "s" }, "<M-j>", function()
         if ls.jumpable(-1) then
           ls.jump(-1)
         end
       end, { silent = true, desc = "LuaSnip jump backward" })
 
-      vim.keymap.set({ "i", "s" }, "<C-l>", function()
+      vim.keymap.set({ "i", "s" }, "<M-l>", function()
         if ls.choice_active() then
           ls.change_choice(1)
         end
       end, { silent = true, desc = "LuaSnip next choice" })
 
-      vim.keymap.set({ "i", "s" }, "<C-h>", function()
+      vim.keymap.set({ "i", "s" }, "<M-h>", function()
         if ls.choice_active() then
           ls.change_choice(-1)
         end
