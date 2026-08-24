1f9c60a6e4272f2bec009a83865d1aadd3139901
Brysen Pfingsten <brysen.pfingsten@gmail.com>
2026-08-24 18:45:30 -0400

    Tuido: git synchorinzation

--- tuido/tuido.nix
+++ tuido/tuido.nix
@@ -3,6 +3,8 @@
     enable = true;
     settings = {
       time.use_12h = true;
+      sync.enabled = true;
+      sync.remote = "https://github.com/brysenPfingsten/todos.git";
       # colors.region_selected = "#ff6600";
       # icons.done = "✓ ";
       # icons.todo = "○ ";
