131e05c5d805e01a8cd6a49f5ff1fb83e167bb6a
Brysen Pfingsten <brysen.pfingsten@gmail.com>
2026-09-23 16:22:44 -0400

    New background

--- niri/startup.nix
+++ niri/startup.nix
@@ -1,7 +1,7 @@
 {
   "spawn-at-startup" = [
     {sh = "awww-daemon";}
-    {sh = "awww img ~/Pictures/gojo-white.jpg";}
+    {sh = "awww img ~/Pictures/nix.jpg";}
     {sh = "clipse -listen";}
     {sh = "sunsetr";}
   ];
